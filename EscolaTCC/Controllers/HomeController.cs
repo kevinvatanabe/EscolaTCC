c738b28d4076891e64913353e084ea82d1bbb7d7
Kevin Vatanabe <kkvatanabek@gmail.com>
2019-06-12 17:07:49 -0300

    Detalhes do Lucas sobre List. Modificação da NavBar.

--- EscolaTCC/Controllers/HomeController.cs
+++ EscolaTCC/Controllers/HomeController.cs
@@ -6,6 +6,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using EscolaTCC.Models.ViewsModels;
 using Microsoft.AspNetCore.Http;
+using MySql.Data.MySqlClient;
+using EscolaTCC.Models.Data;
+using System.Data;
+using EscolaTCC.Models;
 
 namespace EscolaTCC.Controllers
 {
@@ -47,5 +51,28 @@ namespace EscolaTCC.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+
+        public ActionResult Turmas()
+        {
+            Conexao con = new Conexao();
+            MySqlCommand msc = new MySqlCommand("Select * from tblturma",con.conectarBD());
+            MySqlDataAdapter adapter = new MySqlDataAdapter(msc);
+            DataSet ds = new DataSet();
+            adapter.Fill(ds);
+
+            List<Turma> listTurma = new List<Turma>();
+            foreach(DataRow dr in ds.Tables[0].Rows)
+            {
+                Turma turma = new Turma();
+                turma.cdTurma = int.Parse(dr["cdTurma"].ToString());
+                turma.nmTurma = dr["cdTurma"].ToString();
+                turma.dsSerie = dr["dsSerie"].ToString();
+                listTurma.Add(turma);
+            }
+
+            return View(listTurma);
+        }
+
     }
 }
